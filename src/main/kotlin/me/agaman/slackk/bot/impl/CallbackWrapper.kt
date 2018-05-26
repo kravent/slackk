b3aa04a6b47e39bd8e7c1c8fd0c38b675ffce2b0
Adrián García <dev@agaman.me>
2018-05-26 14:34:40 +0200

    Set name for callback coroutines

--- src/main/kotlin/me/agaman/slackk/bot/impl/CallbackWrapper.kt
+++ src/main/kotlin/me/agaman/slackk/bot/impl/CallbackWrapper.kt
@@ -1,14 +1,16 @@
 package me.agaman.slackk.bot.impl
 
+import kotlinx.coroutines.experimental.CoroutineName
 import kotlinx.coroutines.experimental.launch
 import mu.KotlinLogging
 
 private const val ERROR_MESSAGE = "Error thrown in Slackk callback"
 
+private val slackkCoroutineContext = CoroutineName("slackk")
 private val logger = KotlinLogging.logger {}
 
 internal inline fun wrapSlackkCallback(crossinline job: () -> Unit) = {
-    launch {
+    launch(slackkCoroutineContext) {
         try {
             job()
         } catch (t: Throwable) {
@@ -18,7 +20,7 @@ internal inline fun wrapSlackkCallback(crossinline job: () -> Unit) = {
 }
 
 internal inline fun <reified T> wrapSlackkCallback(crossinline job: (T) -> Unit) = { param: T ->
-    launch {
+    launch(slackkCoroutineContext) {
         try {
             job(param)
         } catch (t: Throwable) {
