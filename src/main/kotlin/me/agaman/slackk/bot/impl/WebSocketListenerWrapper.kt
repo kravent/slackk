6e84b4eef74db6775dd8360a05ed95d6d287747a
Adrián García <dev@agaman.me>
2018-05-31 00:00:00 +0200

    Fixed WS listener wrapper

--- src/main/kotlin/me/agaman/slackk/bot/impl/WebSocketListenerWrapper.kt
+++ src/main/kotlin/me/agaman/slackk/bot/impl/WebSocketListenerWrapper.kt
@@ -5,31 +5,30 @@ import okhttp3.WebSocket
 import okhttp3.WebSocketListener
 
 internal class WebSocketListenerWrapper(
-        private var onOpen: (() -> Unit)? = null,
+        private var onOpen: ((Response) -> Unit)? = null,
         private var onMessage: ((String) -> Unit)? = null,
-        private var onClosing: (() -> Unit)? = null,
-        private var onClosed: (() -> Unit)? = null,
-        private var onFailure: (() -> Unit)? = null
+        private var onClosing: ((String) -> Unit)? = null,
+        private var onClosed: ((String) -> Unit)? = null,
+        private var onFailure: ((Throwable, Response?) -> Unit)? = null
 ) : WebSocketListener() {
 
-    override fun onOpen(webSocket: WebSocket?, response: Response?) {
-        onOpen?.invoke()
+    override fun onOpen(webSocket: WebSocket, response: Response) {
+        onOpen?.invoke(response)
     }
 
     override fun onMessage(webSocket: WebSocket, text: String) {
         onMessage?.invoke(text)
     }
 
-    override fun onClosing(webSocket: WebSocket?, code: Int, reason: String?) {
-        onClosing?.invoke()
+    override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+        onClosing?.invoke(reason)
     }
 
-    override fun onClosed(webSocket: WebSocket?, code: Int, reason: String?) {
-        onClosed?.invoke()
+    override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
+        onClosed?.invoke(reason)
     }
 
-    override fun onFailure(webSocket: WebSocket?, t: Throwable?, response: Response?) {
-        t?.let { throw it } // TODO remove debug line
-        onFailure?.invoke()
+    override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+        onFailure?.invoke(t, response)
     }
 }
