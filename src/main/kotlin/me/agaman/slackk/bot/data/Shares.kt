608658fc24b9a68dd42876afd1f644ac98d21c23
agsantiago <agsantiago@tuenti.com>
2018-10-11 20:02:07 +0200

    Fixed Shares structure

--- src/main/kotlin/me/agaman/slackk/bot/data/Shares.kt
+++ src/main/kotlin/me/agaman/slackk/bot/data/Shares.kt
@@ -1,6 +1,6 @@
 package me.agaman.slackk.bot.data
 
 data class Shares(
-        val public: Map<String, Share> = emptyMap(),
-        val private: Map<String, Share> = emptyMap()
+        val public: Map<String, List<Share>> = emptyMap(),
+        val private: Map<String, List<Share>> = emptyMap()
 )
