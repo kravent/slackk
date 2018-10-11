aa3e373018307057a2aeb062b9b35f2af7d47f65
agsantiago <agsantiago@tuenti.com>
2018-10-11 14:14:30 +0200

    Make data class the user events

--- src/main/kotlin/me/agaman/slackk/bot/event/TeamJoinEvent.kt
+++ src/main/kotlin/me/agaman/slackk/bot/event/TeamJoinEvent.kt
@@ -3,6 +3,6 @@ package me.agaman.slackk.bot.event
 import me.agaman.slackk.bot.data.User
 
 @EventType("team_join")
-class TeamJoinEvent(
+data class TeamJoinEvent(
         val user: User
 ) : Event()
--- src/main/kotlin/me/agaman/slackk/bot/event/UserChangeEvent.kt
+++ src/main/kotlin/me/agaman/slackk/bot/event/UserChangeEvent.kt
@@ -3,6 +3,6 @@ package me.agaman.slackk.bot.event
 import me.agaman.slackk.bot.data.User
 
 @EventType("user_change")
-class UserChangeEvent(
+data class UserChangeEvent(
         val user: User
 ) : Event()
